2ad2645e1f7de5d2ab71a6622f07f83595f2592f
DarthBenro008 <hkpdev008@gmail.com>
2021-06-28 00:44:31 +0530

    add: splash screen

---
+++ app/src/main/java/com/benrostudios/enchante/SplashActivity.kt
@@ -0,0 +1,51 @@
+package com.benrostudios.enchante
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.benrostudios.enchante.ui.AuthActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash)
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+
+                if (FirebaseAuth.getInstance().currentUser != null) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    this.finish()
+                } else {
+                    startActivity(Intent(this, AuthActivity::class.java))
+                    this.finish()
+                }
+
+
+            }, SPLASH_TIME_OUT
+        )
+    }
+
+    companion object {
+        private const val SPLASH_TIME_OUT = 500L
+
+    }
+}
